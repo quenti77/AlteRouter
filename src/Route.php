ccf140c6d4bccf1477c2e75096941ede8cc51bf6
Quentin <quentin.ysambert@gmail.com>
2023-04-10 15:35:10 +0200

    feat(AR-1): add routes in router with standard and helpers (#2)

--- src/Route.php
+++ src/Route.php
@@ -4,4 +4,40 @@ namespace Alterouter;
 
 class Route
 {
+    /** @var callable|string $handler */
+    private $handler;
+
+    public function __construct(
+        private readonly string $method,
+        private readonly string $path,
+        callable|string $handler,
+        private readonly ?string $name = null,
+    ) {
+        $this->handler = $handler;
+    }
+
+    public function getMethod(): string
+    {
+        return $this->method;
+    }
+
+    public function getPath(): string
+    {
+        return $this->path;
+    }
+
+    public function getHandler(): callable|string
+    {
+        return $this->handler;
+    }
+
+    public function isCallable(): bool
+    {
+        return is_callable($this->handler);
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
 }
